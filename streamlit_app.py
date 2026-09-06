ef7038ff24092eef00a2315850986d914f8a66a3
cheikhfallsyr <chfall@syr.edu>
2026-09-06 21:24:58 +0000

    modified streamlit_app modified HW1 modified HW2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,80 +1,9 @@
-from pypdf import PdfReader
 import streamlit as st
-from openai import OpenAI
 
-def read_pdf(uploaded_file):
-    reader = PdfReader(uploaded_file)
-    return "\n".join(page.extract_text() or "" for page in reader.pages)
+st.title("HW Manager")
 
-# Show title and description.
-st.title("📄 Document question answering")
-st.write(
-    "Upload a document below and ask a question about it – GPT will answer! "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-)
+hw1 = st.Page("HW/HW1.py", title="HW 1")
+hw2 = st.Page("HW/HW2.py", title="HW 2", default=True)
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
-
-    # Create an OpenAI client.
-    try:
-        client = OpenAI(api_key=openai_api_key)
-        client.models.list()
-    except Exception:
-        st.error("Invalid OpenAI Key. Please enter a valid key")
-        st.stop()
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.pdf or .txt)", type=("pdf", "txt")
-    )
-    # select a model
-    model_name = st.selectbox(
-        "Select a model",
-        (
-            "gpt-3.5-turbo",
-            "gpt-4.1",
-            "gpt-5-chat-latest",
-            "gpt-5-nano",
-        ),
-    )
-
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
-
-    if uploaded_file and question:
-
-        # Process the uploaded file and question.
-        file_extension = uploaded_file.name.split(".")[-1]
-
-        if file_extension == "txt":
-            document = uploaded_file.read().decode()
-        elif file_extension == "pdf":
-            document = read_pdf(uploaded_file)
-        else:
-            st.error("Unsupported file type.")
-       
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
-
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model=model_name,
-            messages=messages,
-            stream=True,
-        )
-
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+selected_page = st.navigation([hw1, hw2])
+selected_page.run()
